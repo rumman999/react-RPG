80e14dda9fc58c15c83a1fb83b024ac9ff61aec7
rumman999 <rumman.rk17@gmail.com>
2025-06-16 07:58:59 +0600

    Updates to vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/rumman999/react-RPG',
+  base: '/react-RPG/',
   plugins: [react(),tailwindcss()],
   
 })
